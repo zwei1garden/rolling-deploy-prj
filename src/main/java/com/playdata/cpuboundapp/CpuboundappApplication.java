3a4cc893a7228225dd8adab9f3113a778f5a05b3
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-30 14:42:57 +0900

    전체 프로젝트 업로드

---
+++ src/main/java/com/playdata/cpuboundapp/CpuboundappApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.cpuboundapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpuboundappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpuboundappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playdata/cpuboundapp/CpuboundappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playdata.cpuboundapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpuboundappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
