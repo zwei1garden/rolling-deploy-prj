bc2410a015084384670d0aaea127c1cbe3a84af3
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-31 12:10:43 +0900

    파이프라인 한 번 더 확인

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test + build test no sudo message + socket permission + auto image upload + update password";
+        return "webhook docker test + build test no sudo message + socket permission + auto image upload + update password + one more time check";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
