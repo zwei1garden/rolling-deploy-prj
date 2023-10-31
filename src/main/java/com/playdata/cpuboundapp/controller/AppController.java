1fe21a89629ad3ca96174ac2111e69ec0e984d74
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-31 11:22:54 +0900

    빌드부터 하도록 수정

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test + build test no sudo message + socker permission";
+        return "webhook docker test + build test no sudo message + socket permission";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
