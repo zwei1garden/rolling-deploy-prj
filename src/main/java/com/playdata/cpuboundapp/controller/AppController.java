73a2d595189948361320605fe3e6c054a011e967
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-31 10:47:46 +0900

    이미지 빌드 되느지 테스트

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test";
+        return "webhook docker test + build test";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
