c8017f7edb52c224c7bdded165183db718a019bc
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-31 11:16:01 +0900

    docker 권한 666수정완료

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test + build test no sudo message";
+        return "webhook docker test + build test no sudo message + socker permission";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
