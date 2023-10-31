3fa414db792ca48b097758caf6dece5edada62d1
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-31 11:05:25 +0900

    sudo 뺴고 갑니다

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook docker test + build test";
+        return "webhook docker test + build test no sudo message";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
