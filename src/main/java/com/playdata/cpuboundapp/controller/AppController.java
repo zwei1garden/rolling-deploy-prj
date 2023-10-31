78e844f153a8e5bcf1dc1ddde321b06544d16c0d
dev.rary@gmail.com <dev.rary@gmail.com>
2023-10-31 10:35:25 +0900

    webhook 빌드 유발 테스트1

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ci/cd test rolling deploy test";
+        return "webhook docker test";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
