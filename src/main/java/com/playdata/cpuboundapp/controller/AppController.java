d6dbc903d447f0554893b083e7380c3c3d0d1a3d
dev.rary@gmail.com <dev.rary@gmail.com>
2023-11-01 09:42:38 +0900

    attachmode test

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook test restart!";
+        return "webhook test restart!!!";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
