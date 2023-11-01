aaf740ed615978e87429286080458f8a23cd1692
dev.rary@gmail.com <dev.rary@gmail.com>
2023-11-01 10:27:15 +0900

    무중단 배포 문제점

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook test restart!!!!! detach mode";
+        return "webhook test restart!!!!! detach mode rolling deploy";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
