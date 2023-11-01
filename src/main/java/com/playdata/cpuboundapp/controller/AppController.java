a356828a1168c705e8d048b670e959e6146f08d4
dev.rary@gmail.com <dev.rary@gmail.com>
2023-11-01 10:34:06 +0900

    sleep을 이용한 시차 배포

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "webhook test restart!!!!! detach mode rolling deploy";
+        return "webhook test restart!!!!! detach mode rolling deploy, sleep 60";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
