7d1f92ec83d9106498262ce499919986f15e012b
Lee Jeong Won <106802869+zwei1garden@users.noreply.github.com>
2024-07-11 14:33:46 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "bye~ leegarden nhn cloud~";
+        return "hello~ leegarden nhn cloud~";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
