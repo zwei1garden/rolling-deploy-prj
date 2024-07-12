4447a9c195c69be1b6cffbda9060e3b73f992d67
Lee Jeong Won <106802869+zwei1garden@users.noreply.github.com>
2024-07-12 14:01:11 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -19,7 +19,7 @@ public class AppController {
         return input;
     }
 
-    @RequestMapping("/hello")
+    // @RequestMapping("/hello")
     public String hello() {
         return "hello~ leegarden nhn cloud~ success!";
     }
